84da57acd6c2d08cead21bc0e6185f257f72788c
ben <13510800329@163.com>
2023-08-04 13:46:36 +0800

    v1.2

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -3,17 +3,12 @@ package com.gupao.springbootdemo.controller;
 import jdk.nashorn.internal.runtime.logging.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.InetAddress;
-
 @RestController
 @Logger
 public class K8SController {
-
-
     @RequestMapping("/k8s")
     public String k8s(){
-        System.out.println("hello ##eeeeeeeeee33333333##world.......");
+        System.out.println("hello,my jenkins.");
         return "hello K8s <br/>111222 ";
     }
 }

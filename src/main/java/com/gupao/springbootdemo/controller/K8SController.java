f148a6c18a8e493712f4154e9efcda15b4f1c47f
ben <13510800329@163.com>
2023-08-04 15:02:14 +0800

    v1.6

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -9,6 +9,6 @@ public class K8SController {
     @RequestMapping("/k8s")
     public String k8s(){
         System.out.println("hello,my jenkins.");
-        return "hello jenkinsjfddsfsdfsfsfhhh98113333311ddddd1";
+        return "hello jenkinsOK";
     }
 }
